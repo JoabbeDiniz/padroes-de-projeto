788181132cd95b7b17031859459b3b30d8f70fc9
Joabbe Jovanni <jojo@MacBook-Air-de-Joabbe.local>
2025-08-26 16:30:43 -0300

    adapter

---
+++ estrutural/br/edu/ifs/designpatterns/adapter/impl/AdaptadorLista.java
@@ -0,0 +1,35 @@
+package br.edu.ifs.designpatterns.adapter.impl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import br.edu.ifs.designpatterns.adapter.Repositorio;
+import br.edu.ifs.designpatterns.adapter.RepositorioLista;
+
+public class AdaptadorLista implements Repositorio {
+
+    private RepositorioLista repositorio;
+
+    public AdaptadorLista(RepositorioLista repositorio) {
+        this.repositorio = repositorio;
+    }
+
+    public Map<String, String> obterAlunos() {
+        Map<String, String> mapa = new HashMap<>();
+        List<String> linhas = repositorio.recuperarDados();
+
+        if (linhas != null && !linhas.isEmpty()) {
+            for (String linha : linhas) {
+                String[] partes = linha.split("\t", 2);
+                if (partes.length == 2) {
+                    String matricula = partes[0].trim();
+                    String nome = partes[1].trim();
+                    mapa.put(matricula, nome);
+                }
+            }
+        }
+
+        return mapa;
+    }
+}
---
+++ estrutural/br/edu/ifs/designpatterns/adapter/impl/AdaptadorString.java
@@ -0,0 +1,34 @@
+package br.edu.ifs.designpatterns.adapter.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import br.edu.ifs.designpatterns.adapter.Repositorio;
+import br.edu.ifs.designpatterns.adapter.RepositorioString;
+
+public class AdaptadorString implements Repositorio {
+
+    private RepositorioString repositorio;
+
+    public AdaptadorString(RepositorioString repositorio) {
+        this.repositorio = repositorio;
+    }
+
+    public Map<String, String> obterAlunos() {
+        Map<String, String> mapa = new HashMap<>();
+        String dados = repositorio.recuperarDados();
+
+        if (dados != null && !dados.isBlank()) {
+            String[] linhas = dados.split(";");
+            for (String linha : linhas) {
+                String[] partes = linha.split("\t", 2);
+                if (partes.length == 2) {
+                    String matricula = partes[0].trim();
+                    String nome = partes[1].trim();
+                    mapa.put(matricula, nome);
+                }
+            }
+        }
+        return mapa;
+    }
+}
